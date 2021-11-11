fe3fe8aab68d20f38dc03b4a1a0551edd40de1b3
Ldeng928 <lideng928@gmail.com>
2021-11-11 16:20:25 +0000

    Add assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const greenEmoji = String.fromCodePoint(9989);
+const redEmoji = String.fromCodePoint(10060);
+
+const assertEqual = (actual, expected) => {
+    if(actual === expected) {
+        console.log(`${greenEmoji}${greenEmoji}${greenEmoji} Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`${redEmoji}${redEmoji}${redEmoji} Assertion Failed: ${actual} !== ${expected}`);
+    }
+}
+
+// Test assertEqual function
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual('hello', 'hello');
+assertEqual('lighthouselab', 'bootstrap');
+assertEqual(1, 'lighthouselab');
