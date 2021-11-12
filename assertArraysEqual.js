cc4becbd2c1c7a2ceb7f0396989303f9b610363b
Ldeng928 <lideng928@gmail.com>
2021-11-12 19:35:33 +0000

    Add assertArraysEqual function and update eqArray function

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = require('./eqArrays');
+
+const greenEmoji = String.fromCodePoint(9989);
+const redEmoji = String.fromCodePoint(10060);
+
+const assertArraysEqual = (arr1, arr2) => {
+  if(eqArrays(arr1, arr2)) {
+      console.log(`${greenEmoji}${greenEmoji}${greenEmoji} Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+      console.log(`${redEmoji}${redEmoji}${redEmoji} Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+// Test with assertArraysEqual function
+assertArraysEqual([1, 3, 4], [2, 3, 4]);
+assertArraysEqual([1, 3, 4], [1, 3, 4]);
+assertArraysEqual([1, 3, 4], ['1', 3, 4]);
