4df609246e88eec7583fc5cd00809f394c4595db
Ldeng928 <lideng928@gmail.com>
2021-11-25 16:05:24 +0000

    Added Mocha and Chai as dev dependencies for testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,23 @@
-const assertEqual = require('../assertEqual');
+/* Use Mocha for testing */
+const assert = require('chai').assert;
 const head = require('../head');
-// Test the head function with assertEqual
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello', 'Lighthosue', 'Labs']), 'Hello');
-assertEqual(head([5,6,7]), 9);
+const testArrayOne = [1, 2, 3];
+const testArrayTwo = [5];
+const testArrayThree = ['Hello', 'Lighthosue', 'Labs'];
+
+
+describe('#head', () => {
+  it('should return 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head(testArrayOne), 1);
+  });
+
+  it('should return 5 for [5]', () => {
+    assert.strictEqual(head(testArrayTwo), 5);
+  });
+
+  it('should return Hello for ["Hello", "Lighthosue", "Labs"]', () => {
+    assert.strictEqual(head(testArrayThree), 'Hello')
+  });
+
+
+});
